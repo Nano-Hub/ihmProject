971cf9adfbdcb0ed2e97892070715e84569bb506
Nano-Hub <stefano.alder@telecom-st-etienne.fr>
2017-01-30 11:46:37 +0100

    creation connexion et ajout page de base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,35 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ConnexionPage } from '../pages/connexion/connexion';
+import { ProfilPage } from '../pages/profil/profil';
+import { BonsPage } from '../pages/bons/bons';
+
 
 @NgModule({
   declarations: [
+    ConnexionPage,
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ProfilPage,
+    BonsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
+    ConnexionPage,
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    BonsPage,
+    ProfilPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
---
+++ src/pages/connexion/connexion.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the Connexion page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-connexion',
+  templateUrl: 'connexion.html'
+})
+export class ConnexionPage {
+  tabLog: string = "Connexion";
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams)
+  {
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ConnexionPage');
+  }
+
+}
